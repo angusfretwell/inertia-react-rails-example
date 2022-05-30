4433afdb9d716b6b2e1031a84f773fa66cf38fee
Angus Fretwell <angusfretwell@me.com>
2022-05-30 19:59:46 +1000

    scaffold articles and install vite react

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import RubyPlugin from "vite-plugin-ruby";
 import ReactPlugin from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [RubyPlugin()], //  ReactPlugin()
+  plugins: [RubyPlugin(), ReactPlugin()],
 });
